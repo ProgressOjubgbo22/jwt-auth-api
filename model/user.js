e6c36502b515442f7ec37e146bb7ee39475f800b
Ojugbo Progress <lingardj852@gmail.com>
2026-05-14 13:22:49 -0700

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,45 +1,29 @@
-const jwt = require('jsonwebtoken');
-const User = require('../model/user');
+const mongoose = require('mongoose');
 
-const secretkey = "secretKey";
-
-async function auth (req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if(!token) {
-        return res.status(401).json({message: 'access denied;'});
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }, 
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
     }
+}, {timestamps: true});
 
-        try{
+const User = mongoose.model('User', userSchema);
 
-            const decoded = jwt.verify(token, secretkey);
-
-            const foundUser =  await User.findById(decoded.id);
-            if(!foundUser) {
-                return res.status(404).json({message: "user not found"});
-            }
-            req.user = foundUser;
-            next();
-        }
-        catch(err) {
-            console.error(err);
-            return res.status(500).json({message: "server error"});
-            
-        }
-    
-
-}
-
-function generateToken(id) {
-    return jwt.sign({ id }, secretkey, { expiresIn: '1h' });
-}
-
-function authAdmin(req, res, next) {
-    if(req.user.role !== 'admin') {
-        return res.status(403).json({message: "access denied"});
-    }
-    next();
-}
-
-module.exports = {auth, generateToken, authAdmin};
+module.exports = User;
